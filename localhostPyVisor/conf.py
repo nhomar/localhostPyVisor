d8574136c2fb0a334ddf9235b2e06bae292fda84
Nhomar Hernandez <nhomar@gmail.com>
2014-09-27 19:03:36 -0430

    [SHOW]

--- localhostPyVisor/conf.py
+++ localhostPyVisor/conf.py
@@ -1,7 +1,12 @@
 # configuration
 DATABASE = '/tmp/flaskr.db'
+#Local mail folder
+# /var/mail byt default on macOsX
+MAILFOLDER = '/var/mail'
 DEBUG = True
 STATIC_FOLDER = 'static'
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
+# mailbox you want read.
+USERNAME = 'nhomar'
+# *nix password if False plain text if setted
 PASSWORD = 'default'
--- localhostPyVisor/localhostPyVisor.py
+++ localhostPyVisor/localhostPyVisor.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import mailbox
 
 from functools import wraps
-from flask import request, Response, Flask, render_template, send_from_directory
+from flask import request, session, flash, Response, redirect, url_for, Flask, render_template, send_from_directory
 
 from . import conf
 
 app = Flask(__name__)
 app.config.from_object(conf)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return ''
 
 def requires_auth(f):
     @wraps(f)
@@ -24,16 +20,27 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-@app.route('/secret-page')
+def _entries():
+    fol = app.config['MAILFOLDER']
+    usr = app.config['USERNAME']
+    fullpath = os.path.join(fol, usr)
+    entries = []
+    for m in mailbox.mbox(fullpath):
+        print type(m)
+        entry = m
+        entries.append(entry)
+    return entries
+
+@app.route('/')
 @requires_auth
-def secret_page():
-    return render_template('secret_page.html')
+def catch_all():
+    return render_template('show_entries.html', entries=_entries(), dir=dir)
 
 def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == app.config['USERNAME'] and password == app.config['PASSWORD']
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
@@ -47,3 +54,22 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('/'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
